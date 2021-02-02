c860c33db03a23e017425bb815a9ef5a229be0d5
tyler <tyler.pressnell86@gmail.com>
2021-02-01 19:45:43 -0600

    Completed logic for generating story fragments.

--- app/src/main/java/com/example/lab2b/MainActivity.java
+++ app/src/main/java/com/example/lab2b/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.lab2b;
 
 import androidx.appcompat.app.AppCompatActivity;
+import java.util.Random;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String [] fragments;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fragments = getResources().getStringArray(R.array.fragments);
     }
+
+    public void onClickHelp(View V){
+
+        int fragmentNum = fragments.length;
+        Random rand = new Random();
+        int fragmentChoice = rand.nextInt(fragmentNum);
+        String fragment = fragments[fragmentChoice];
+
+        TextView help = (TextView) findViewById(R.id.helpText);
+        help.setText(fragment);
+    }
+
+
 }
